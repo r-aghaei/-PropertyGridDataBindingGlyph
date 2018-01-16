203a688bfbe56f48e5acee1c6aa060254373058e
Reza Aghaei <rag@sitecore.net>
2018-01-16 17:16:52 +0800

    Example Created

--- src/PropertyGridDataBindingGlyph/Form1.Designer.cs
+++ src/PropertyGridDataBindingGlyph/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.exPropertyGrid1 = new PropertyGridDataBindingGlyph.ExPropertyGrid();
+            this.SuspendLayout();
+            // 
+            // exPropertyGrid1
+            // 
+            this.exPropertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.exPropertyGrid1.Location = new System.Drawing.Point(0, 0);
+            this.exPropertyGrid1.Name = "exPropertyGrid1";
+            this.exPropertyGrid1.Size = new System.Drawing.Size(235, 221);
+            this.exPropertyGrid1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 221);
+            this.Controls.Add(this.exPropertyGrid1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ExPropertyGrid exPropertyGrid1;
     }
 }
 
---
+++ src/PropertyGridDataBindingGlyph/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PropertyGridDataBindingGlyph
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.exPropertyGrid1.DataBindings.Add("Anchor", this, "Anchor");
+            this.exPropertyGrid1.DataBindings.Add("BackColor", this, "BackColor");
+            this.exPropertyGrid1.DataBindings.Add("AccessibleRole", this, "AccessibleRole");
+            this.exPropertyGrid1.SelectedObject = exPropertyGrid1;
+        }
+    }
+}
